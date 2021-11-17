9ba328e5402f26176875fb4ccabeedc1bab67913
Maido47 <maid768@gmail.com>
2021-11-17 16:19:33 +0100

    Deleted logout/claims functions from items comp.

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MyAuthService } from '../my-auth.service';
 import { ItemService } from './item.service';
 
 @Component({
@@ -10,43 +11,25 @@ import { ItemService } from './item.service';
 })
 
 export class ItemsComponent implements OnInit {
+  fullName: string = '';
+  subscription: Subscription;
 
-  identityClaim;
-  accesstoken: string;
-  token: string;
-  errorMessage: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  constructor(private oauthService: OAuthService, private itemService: ItemService) { }
+  constructor(private router: Router, private myAuthService: MyAuthService) { }
 
   ngOnInit(): void {
+    this.myAuthService.getUser();
+    this.subscribing();
   }
 
-  login() {
-    this.oauthService.initImplicitFlow();
-    this.getClaims();
+  subscribing(){
+    this.subscription = this.myAuthService.currentUser.subscribe((user) => {
+      this.fullName = user;
+    });
   }
 
   logout() {
-    this.oauthService.logOut();
-    this.itemService.navigateHome();
-  }
-
-  getClaims() {
-    this.accesstoken = JSON.stringify(this.oauthService.getAccessToken());
-    this.token = JSON.stringify(this.oauthService.getIdToken());
-
-    this.identityClaim = this.oauthService.getIdentityClaims();
-    this.email = this.identityClaim.upn;
-
-    var namePart = this.email.substring(0, this.email.indexOf("@")); // get "firstName.lastName"
-    this.firstName = namePart.substring(0, namePart.indexOf(".")); // get first name from name part
-    this.firstName = this.firstName.charAt(0).toUpperCase() + this.firstName.slice(1); // set first letter uppercase for first name
-  	this.lastName = namePart.substring(namePart.lastIndexOf('.') + 1); // get last name from name part
-    this.lastName = this.lastName.charAt(0).toUpperCase() + this.lastName.slice(1); // set first letter uppercase for last name
-
-    return this.itemService.getClaims();
+    this.myAuthService.logout();
+    this.router.navigate(['welcome']);
   }
 
 }
