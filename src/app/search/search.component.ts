6e3227d3ee0cbad68179900db1fd04659d9a62f8
Maido47 <maid768@gmail.com>
2022-01-04 09:55:51 +0100

    Configured datatable options and styling

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ItemSearchService } from '../items/item-search.service';
 import { Item } from '../items/item.model';
 import { ItemService } from '../items/item.service';
 import { MyAuthService } from '../my-auth.service';
+
 
 @Component({
   selector: 'app-search',
@@ -18,9 +20,29 @@ export class SearchComponent implements OnInit {
   searchString: string;
   loadingSpinner = true;
 
+  dtOptions: any;
+
   constructor(private router: Router, private itemService: ItemService, public itemSearchService: ItemSearchService, private myAuthService: MyAuthService) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      lengthMenu: [5,15,25],
+      processing: true,
+      dom: 'Bfrtip',
+	 buttons: {
+      buttons: [
+            { extend: 'excel', className: 'btn btn-outline-success mb-3'},
+            { extend: 'csv', className: 'btn btn-outline-success mb-3' }
+               ],
+       dom: {
+		  button: {
+		  className: 'btn'
+	         }
+       }
+     }
+    }
   }
 
   onSubmit(f: NgForm) {
