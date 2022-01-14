7fbc644fe8089879b71cb453a526336b637812c0
Maido47 <maid768@gmail.com>
2022-01-14 10:25:26 +0100

    Changed search form and search procedure

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import * as bootstrap from 'bootstrap';
 import { Modal } from 'bootstrap';
@@ -19,13 +19,20 @@ import { HttpErrorInterceptorService } from 'src/app/_interceptor/http-error-int
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
 
   searchedItems: Item[] = [];
-  searchString: string;
+  searchString = '';
+  searchCompleted: number;
   body: string;
   testModal: Modal | undefined;
   isError = false;
+  itemDescription = '';
+  itemIsComplete = 0;
+  itemForm = new FormGroup({
+    search: new FormControl(this.itemDescription, Validators.required),
+    isComplete: new FormControl(this.itemIsComplete, Validators.required)
+  });
 
   // variable for letting the user know that data is beeing fetched
   loadingSpinner = true;
@@ -33,9 +40,9 @@ export class SearchComponent implements OnInit{
   // used for configuring the datatable
   dtOptions: any;
 
-  constructor(private router: Router, private itemService: ItemService, 
+  constructor(private router: Router, private itemService: ItemService,
     public itemSearchService: ItemSearchService) {
-     }
+  }
 
   ngOnInit(): void {
     //configuring the settings for datatables with export buttons
@@ -46,9 +53,9 @@ export class SearchComponent implements OnInit{
       lengthMenu: [5, 15, 25],
       processing: true,
       columnDefs: [
-        {"width": "5%", "targets": 0},
-        {"width": "5%", "targets": 2},
-        {"width": "13%", "targets": 3}
+        { "width": "5%", "targets": 0 },
+        { "width": "5%", "targets": 2 },
+        { "width": "13%", "targets": 3 }
       ],
       dom: 'Bfrtip',
       buttons: {
@@ -63,63 +70,68 @@ export class SearchComponent implements OnInit{
         }
       }
     }
-    
+
   }
 
   /*
   * Method to submit the query to in search bar
   * Before displaying the data a loading spinner is activated for 3 seconds
   */
-  onSubmit(f: NgForm) {
-
-    setTimeout(() => {
-      this.loadingSpinner = false;
-    }, 3000);
-    this.searchString = f.value.search;
-    
-    /*
-    * When sending an empty request, AFTER the first search, the searchstring will be 'null' and throws an error
-    * searchstring will be replaced with empty string when searchstring is null to prevent error
-    */
-    if(this.searchString == null || this.searchString == undefined){
-      this.searchString = '';
-    }
-
-    //If searchstring is empty then get the currentitems from itemservice and display all items
-    if (this.searchString.length == 0) {
-      this.itemService.currentItems.subscribe(Response => {
-        this.searchedItems = Response;
-      });
-      //Call the search method from itemsearchservice if searchstring is not empty
-    } else if (this.searchString.length > 0) {
-      this.itemSearchService.search(this.searchString).then(
-        //success
-        ()=> {
-          console.log('suche ausgeführt');
-          f.resetForm();
-        this.itemSearchService.currentSearchedItems.subscribe(Response => {
-        this.searchedItems = Response;
-        });
-        },
-        //error
-        ()=> {
-          console.log('suche nicht ausgeführt, error');
-          this.isError = true;
-          this.loadingSpinner = false;
-          this.openModal();
-        }
-        );
-      
-    }
+  onSubmit() {
 
     //Setting loadingSpinner back to true for every query
     this.loadingSpinner = true;
     this.isError = false;
+
+    setTimeout(() => {
+      this.loadingSpinner = false;
+    }, 3000);
+    this.searchString = this.itemForm.value.search;
+    this.searchCompleted = this.itemForm.value.isComplete;
+
+    //Convert completed value of option element for query-database purposes
+    if (this.searchCompleted == 0) {
+      this.isError = true;
+      this.loadingSpinner = false;
+      this.body = 'Bitte ein Dropdown-Feld auswählen';
+      this.openModal();
+    }
+
+    /*
+    * When clicking in the search bar, searchstring goes from empty string to null string
+    * changing back to empty string to prevent error
+    */
+    if (this.searchString == null || this.searchString == undefined) {
+      this.searchString = '';
+    }
+
+    //Start the search if there is no error
+    if (this.isError == false) {
+      this.itemSearchService.search(this.searchString, this.searchCompleted).then(
+        //success
+        () => {
+          this.itemSearchService.currentSearchedItems.subscribe(Response => {
+            this.searchedItems = Response;
+          });
+        },
+        //error
+        () => {
+          this.isError = true;
+          this.loadingSpinner = false;
+          this.body = 'Suche für \'' + this.searchString + '\' ergab keine Treffer! :(';
+          this.openModal();
+        }
+      );
+    }
+
+    //Resets the search field completely, and the dropdown resets to previous value[0]
+    this.itemForm.reset({ isComplete: 0 });
+
+    
   }
 
   //Method to open the modal, this works
   openModal() {
-    this.body = 'Suche für \'' + this.searchString + '\' ergab keine Treffer! :(';
     this.testModal = new bootstrap.Modal(document.getElementById('testModal'), {
       keyboard: false
     })
